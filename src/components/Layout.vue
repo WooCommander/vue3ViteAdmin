c6bfb9277725ae01173f52206ef550fff83d998a
ocnelavoc <ocnelavoc@gmail.com>
2022-10-05 09:22:35 +0300

    [fix]add content

--- src/components/Layout.vue
+++ src/components/Layout.vue
@@ -101,6 +101,131 @@
                 <i class="fa-regular fa-eye"></i>
               </div>
             </div>
+            <div class="card">
+              <div>
+                <div class="numbers">80</div>
+                <div class="cardName">Sales</div>
+              </div>
+              <div class="iconBox">
+                <i class="fa-solid fa-cart-shopping"></i>
+              </div>
+            </div>
+            <div class="card">
+              <div>
+                <div class="numbers">208</div>
+                <div class="cardName">Comments</div>
+              </div>
+              <div class="iconBox">
+                <i class="fa-solid fa-comment"></i>
+              </div>
+            </div>
+            <div class="card">
+              <div>
+                <div class="numbers">6,042</div>
+                <div class="cardName">Earning</div>
+              </div>
+              <div class="iconBox">
+                <i class="fa-solid fa-dollar-sign"></i>
+              </div>
+            </div>
+          </div>
+          <div class="details">
+            <div class="recentOrders">
+              <div class="cardHeader">
+                <h2>Recent Orders</h2>
+                <a href="#" class="btn">View All</a>
+              </div>
+              <table>
+                <thead>
+                  <tr>
+                    <td>Name</td>
+                    <td>Price</td>
+                    <td>Payments</td>
+                    <td>Status</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>start</td>
+                    <td>$1200</td>
+                    <td>Paid</td>
+                    <td><span class="status delivered">Delivered</span></td>
+                  </tr>
+                  <tr>
+                    <td>start</td>
+                    <td>$1200</td>
+                    <td>Paid</td>
+                    <td><span class="status delivered">Delivered</span></td>
+                  </tr>
+                  <tr>
+                    <td>start</td>
+                    <td>$600</td>
+                    <td>Paid</td>
+                    <td><span class="status">Returned</span></td>
+                  </tr>
+                  <tr>
+                    <td>start</td>
+                    <td>$300</td>
+                    <td>Paid</td>
+                    <td><span class="status inprogress">In progress</span></td>
+                  </tr>
+                  <tr>
+                    <td>start</td>
+                    <td>$300</td>
+                    <td>Paid</td>
+                    <td><span class="status inprogress">In progress</span></td>
+                  </tr>
+                  <tr>
+                    <td>start</td>
+                    <td>$300</td>
+                    <td>Paid</td>
+                    <td><span class="status inprogress">In progress</span></td>
+                  </tr>
+                  <tr>
+                    <td>start</td>
+                    <td>$300</td>
+                    <td>Paid</td>
+                    <td><span class="status inprogress">In progress</span></td>
+                  </tr>
+                  <tr>
+                    <td>start</td>
+                    <td>$300</td>
+                    <td>Paid</td>
+                    <td><span class="status inprogress">In progress</span></td>
+                  </tr>
+                  <tr>
+                    <td>start</td>
+                    <td>$300</td>
+                    <td>Paid</td>
+                    <td><span class="status inprogress">In progress</span></td>
+                  </tr>
+                  <tr>
+                    <td>start</td>
+                    <td>$300</td>
+                    <td>Paid</td>
+                    <td><span class="status inprogress">In progress</span></td>
+                  </tr>
+                  <tr>
+                    <td>start</td>
+                    <td>$300</td>
+                    <td>Paid</td>
+                    <td><span class="status inprogress">In progress</span></td>
+                  </tr>
+                  <tr>
+                    <td>start</td>
+                    <td>$300</td>
+                    <td>Paid</td>
+                    <td><span class="status inprogress">In progress</span></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+            <div class="recentCustomers">
+              <div class="cardHeader">
+                <h2>Recent Customers</h2>
+                <a href="#" class="btn">View All</a>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -298,6 +423,102 @@ const count = ref(0)
             object-fit: cover;
           }
         }
+      }
+
+      .cardBox {
+        position: relative;
+        width: 100%;
+        padding: 20px;
+        display: grid;
+        grid-template-columns: repeat(4, 1fr);
+        grid-gap: 20px;
+
+        .card {
+          position: relative;
+          background-color: #fff;
+          padding: 20px;
+          display: flex;
+          justify-content: space-between;
+          cursor: pointer;
+
+          .numbers {
+            position: relative;
+            font-size: 2em;
+            font-weight: 500;
+
+          }
+
+          .cardName {
+            color: #999;
+          }
+
+          .iconBox {
+            color: #03a9f4;
+          }
+        }
+      }
+
+      .details {
+        position: relative;
+        width: 100%;
+        display: grid;
+        padding: 20px;
+        padding-top: 0;
+        grid-template-columns: 2fr 1fr;
+
+        .recentOrders {
+          position: relative;
+          display: grid;
+          min-height: 530px;
+          background: #fff;
+          padding: 20px;
+
+          .cardHeader {
+            display: flex;
+            justify-content: space-between;
+
+            align-items: flex-start;
+
+            h2 {
+              font-weight: 600;
+            }
+
+            .btn {
+              position: relative;
+              padding: 5px 10px;
+              background: #03a9f4;
+              color: #fff;
+              text-decoration: none;
+              border-radius: 6px;
+
+            }
+          }
+
+          table {
+            width: 100%;
+            border-collapse: collapse;
+            margin-top: 10px;
+
+            thead {
+              td {
+                font-weight: 600;
+
+              }
+            }
+
+            tbody {
+              tr {
+                border-bottom: 1px solid rgba(0, 0, 0, .1);
+
+                &:hover {
+                  background: #03a9f4;
+                  color: #fff;
+                }
+              }
+            }
+          }
+        }
+
       }
     }
   }
