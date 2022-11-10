1324d4a987bf50c443e1572d37aa00ab8572734d
WooCommander <ocnelavoc@gmail.com>
2022-11-10 08:43:02 +0200

    [fix]

--- src/pages/service-section.vue
+++ src/pages/service-section.vue
@@ -1,12 +1,42 @@
 <template>
   <div class="service-section">
-    <div class="box" style="--ctrl:#89ec5b;"></div>
-    <div class="content">
-      <div class="icon"></div>
-      <div class="text">
-        <h3></h3>
-        <p></p>
-        <a href="#"></a>
+    <div class="box" style="--ctrl:#89ec5b;">
+      <div class="content ">
+        <div class="icon"></div>
+        <div class="text">
+          <h3>Developement</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut corporis aspernatur dignissimos. Nesciunt nam
+            sint quaerat deserunt! Vitae itaque eligendi voluptatem, maxime, illum aperiam unde necessitatibus neque
+            quos
+            vero nobis!</p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+    </div>
+    <div class="box" style="--ctrl:#89ec5b;">
+      <div class="content ">
+        <div class="icon"></div>
+        <div class="text">
+          <h3>Developement</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut corporis aspernatur dignissimos. Nesciunt nam
+            sint quaerat deserunt! Vitae itaque eligendi voluptatem, maxime, illum aperiam unde necessitatibus neque
+            quos
+            vero nobis!</p>
+          <a href="#">Read More</a>
+        </div>
+      </div>
+    </div>
+    <div class="box" style="--ctrl:#89ec5b;">
+      <div class="content ">
+        <div class="icon"></div>
+        <div class="text">
+          <h3>Developement</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut corporis aspernatur dignissimos. Nesciunt nam
+            sint quaerat deserunt! Vitae itaque eligendi voluptatem, maxime, illum aperiam unde necessitatibus neque
+            quos
+            vero nobis!</p>
+          <a href="#">Read More</a>
+        </div>
       </div>
     </div>
   </div>
@@ -24,6 +54,8 @@ const count = ref(0)
 </script>
 
 <style lang="less">
-.service-section {}
+.service-section {
+  margin: 10px;
+}
 </style>
   
