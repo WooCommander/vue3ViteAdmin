23d179d8efff93c2fa4f92d54a4485cec11a6aa4
WooCommander <ocnelavoc@gmail.com>
2023-10-05 07:19:13 +0300

    upd

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,30 @@
   </div>
 </template>
 <script setup leng="ts">
-// import { onMounted, vue } from "vue"
+import { onMounted } from "vue"
+import { WebSocketClient } from "ws";
 
-// onMounted(() => {
+onMounted(() => {
 
+  f1()
 
-// })
+})
+const f1 = (() => {
+  const socket = new WebSocket("ws://localhost:8080");
+  // var WebSocketClient = require('websocket').client;
+  // const socket = new WebSocketClient('ws://localhost:8080');
+
+  // Connection opened
+  socket.addEventListener('open', function (event) {
+    socket.send('Hello Server!');
+  });
+
+  // Listen for messages
+  socket.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+  });
+
+})
 
 
 </script>
